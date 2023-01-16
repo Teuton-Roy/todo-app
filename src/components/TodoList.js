15f5ac5bb52dd9ba504cf09b16a8f19c536d85bc
Teuton Roy <popeyehk404@gmail.com>
2023-01-16 22:41:07 +0530

    adding some properties in todolist.js and todolist.css

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -8,7 +8,7 @@ const TodoList = () => {
         <>
             <div className='header'>
                 <h3>Todo List</h3>
-                <button className='btn btn-primary'>Create Task</button>
+                <button className='btn btn-primary mt-2'>Create Task</button>
             </div>
 
             <div className="Task">
